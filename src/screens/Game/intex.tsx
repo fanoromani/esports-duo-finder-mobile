8f62820191a25f17bd647fbf93314c5f6cf4fbac
Stefano Romani <fanoromani@gmail.com>
2022-10-05 13:27:10 -0300

    routes implementation

--- src/screens/Game/intex.tsx
+++
@@ -1,7 +0,0 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-
-import { styles } from "./styles";
-
-export function Game() {
-  return <SafeAreaView style={styles.container}></SafeAreaView>;
-}
