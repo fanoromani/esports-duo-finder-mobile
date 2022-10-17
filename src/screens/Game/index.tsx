0d3773fdb9e148c07a0315d5b90cd5b77b86bd8d
Stefano Romani <fanoromani@gmail.com>
2022-10-17 20:55:04 -0300

    Duo Match Modal

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -11,15 +11,25 @@ import { GameParams } from "../../@types/navigation";
 import { THEME } from "../../theme";
 import { Heading } from "../../components/Heading";
 import { DuoCard, DuoCardProps } from "../../components/DuoCard";
+import { DuoMatch } from "../../components/DuoMatch";
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([]);
+  const [discordDuoSelected, setDiscordDuoSelected] = useState("");
   const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://192.168.15.7:3333/ads/${adsId}/discord`)
+      .then((response) => response.json())
+      .then((data) => {
+        setDiscordDuoSelected(data.discord);
+      });
   }
 
   useEffect(() => {
@@ -57,7 +67,7 @@ export function Game() {
             data={duos}
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
-              <DuoCard onConnect={() => {}} data={duos[0]} />
+              <DuoCard onConnect={() => getDiscordUser(item.id)} data={item} />
             )}
             horizontal
             style={styles.containerList}
@@ -70,6 +80,11 @@ export function Game() {
                 Não há anúncios para este jogo
               </Text>
             )}
+          />
+          <DuoMatch
+            visible={discordDuoSelected.length > 0}
+            discord="Fano#6464"
+            onClose={() => setDiscordDuoSelected("")}
           />
         </SafeAreaView>
       </Text>
